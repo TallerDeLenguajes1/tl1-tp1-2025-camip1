4ffc7d1095f891d9930e87929efacf41974930e6
Camila Perez <camilap11517@gmail.com>
2025-04-01 01:02:34 -0300

    segundo commit archivo .c

--- tp1_1.c
+++ tp1_1.c
@@ -2,5 +2,15 @@
 
 int main(){
     printf("hola mundo");
+
+    //Punto 3e
+    int variable=100;
+    int *punt=&variable;
+    printf("\nContenido del puntero: %d", punt);
+    printf("\nDireccion de memoria almacenada por el puntero: %p", punt);
+    printf("\nDireccion de memoria de la variable: %p", &variable);
+    printf("\nDireccion de memoria del puntero: %p", &punt);
+    printf("\nTamaño de memoria utilizado por la variable: %d bytes", sizeof(variable));
+
     return 0;
 }
